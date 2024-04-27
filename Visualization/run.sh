ee914ad8a4f17a81da7815ea36aa5597348f5a2f
kira1433 <krovidiaashishchandra@gmail.com>
2024-04-28 03:33:54 +0530

    Visual Changes

--- Visualization/run.sh
+++ Visualization/run.sh
@@ -4,6 +4,3 @@
 emcc main.cpp -s WASM=1 -o output.js \
   -s "EXPORTED_RUNTIME_METHODS=['cwrap','UTF8ToString','lengthBytesUTF8']" \
   -s "EXPORTED_FUNCTIONS=['_free', '_malloc','_rna_folding_c']" &&
-
-# Run a local server
-http-server
